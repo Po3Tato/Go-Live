5fa4a790c996df6e2b291f19900eecf8d32a2d76
Po3Tato <31327412+Po3Tato@users.noreply.github.com>
2023-09-07 14:55:56 -0500

    Updated the process of how applications are opened by going to o the installation folder and running the .exe

--- Auto Click.py
+++ Auto Click.py
@@ -1,20 +1,47 @@
+#import modules
+import subprocess
 import os
+import time
+import ctypes
 import ctypes.wintypes
 import pyautogui
-import time
 
-def search_and_open_app(app_name):
-    # Search for the app in the C: drive
-    for root, dirs, files in os.walk('C:\\'):
-        if app_name in files:
-            # Found the app - open it using the default application
-            os.startfile(os.path.join(root, app_name))
-            return
 
-# Open applications
-search_and_open_app("app_name.lnk") # this has to be an icon on the screen
+# specify folder where all .exe are found
+folder_path1 = " "#app folder location goes here
 
-time.sleep(5)
+# First App
+app_name1 = " "#app name goes here. It is the .exe file you want to run
+
+# combine folder and app_name1 for first app
+app_path = os.path.join(folder_path1, app_name1)
+
+try:
+    # Open the application.
+    subprocess.Popen(app_path)
+except FileNotFoundError:
+    print(f"The application '{app_path}' was not found.")
+except Exception as e:
+    print(f"An error occurred: {e}")
+
+# Second App
+folder_path2 = " "#app folder location goes here
+
+# specify the second app to be opened
+app_name2 = " "#app name goes here. It is the .exe file you want to run
+
+# combine folder and app_name2 for first app
+app_path2 = os.path.join(folder_path2, app_name2)
+
+try:
+    # Open the application.
+    subprocess.Popen(app_path2)
+except FileNotFoundError:
+    print(f"The application '{app_path2}' was not found.")
+except Exception as e:
+    print(f"An error occurred: {e}")
+
+time.sleep(4)
 
 # Get the active window handles.
 hwnds = []
@@ -40,11 +67,11 @@ for hwnd in hwnds:
         ctypes.windll.user32.GetWindowRect(hwnd, ctypes.byref(rect))
         ctypes.windll.user32.MoveWindow(hwnd, 1043, 406, 882, 639, True)
 
-time.sleep(5)
+time.sleep(2)
 
 # this clicks on the screen at the specified coordinates
 pyautogui.click(959, 674, button="left")
 
-time.sleep(5)
+time.sleep(2)
 
 sys.exit()
