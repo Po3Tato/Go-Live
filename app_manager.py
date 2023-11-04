71a31d9546b9a5a0876be386322033753e03193a
Jude <31327412+Po3Tato@users.noreply.github.com>
2023-11-03 21:31:18 -0500

    updated auto click app
    added more .py modules for easier code management

---
+++ app_manager.py
@@ -0,0 +1,27 @@
+# import modules
+import os
+import subprocess
+
+def find_app(app_name):
+    search_directories = [
+        "C:\\Program Files",
+        "C:\\Program Files (x86)",
+        os.path.expanduser("~\\AppData\\Local\\Programs"),
+        os.path.expanduser("~\\AppData\\Roaming")
+    ]
+    for directory in search_directories:
+        for root, dirs, files in os.walk(directory):
+            for file in files:
+                if file.lower() == app_name.lower():
+                    return os.path.join(root, file)
+    return None
+
+def open_app(app_name):
+    app_path = find_app(app_name)
+    if app_path is None:
+        print(f"The application '{app_name}' was not found.")
+        return
+    try:
+        subprocess.Popen(app_path)
+    except Exception as e:
+        print(f"An error occurred: {e}")
